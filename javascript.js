c4783fb473447816e6df5eb7dc7002a5888f203d
Mattias Stjernstedt <mstjernstedt@gmail.com>
2021-07-29 15:21:04 +0900

    made deck class with shuffle and sort functionality, added transitions on cards

--- javascript.js
+++ javascript.js
@@ -1,6 +1,46 @@
+class Deck {
+	constructor() {
+		this.cards = [];
+		this.shuffle = function () {
+			// Fisher-Yates shuffle algorithm
+			for (let i = this.cards.length - 1; i > 0; i--) {
+				let j = Math.floor(Math.random() * (i + 1));
+				[this.cards[i], this.cards[j]] = [this.cards[j], this.cards[i]];
+			}
+		};
+
+		this.sortCards = function () {
+			this.cards.sort(function (a, b) { return a.id - b.id; });
+		};
+
+		this.addCard = function (card) {
+			this.cards.push(card);
+		};
+
+		this.popCard = function () {
+			if (this.cards.length == 0)
+				return false;
+			return this.cards.pop();
+		};
+
+		this.getCard = function (card) {
+			return this.cards.splice(this.cards.indexOf(card), 1);
+		};
+
+		this.fillDeck = function () {
+			for (let color = 1; color <= 4; color++) {
+				for (let i = 1; i <= 13; i++) {
+					this.addCard(createCard(color, i));
+				}
+			}
+		};
+	}
+}
+
 function createCard(color, number) {
 	const card = document.createElement("div");
 	card.classList.add("card");
+	card.id = number + (color * 13);
 	card.number = number;
 	card.color = color;
 	switch (color) {
@@ -23,29 +63,17 @@ function createCard(color, number) {
 	return card;
 }
 
-let deck = [];
-let hand = [];
-let board = [];
-
-function createDeck() {
-	for (let color = 1; color <= 4; color++) {
-		for (let i = 1; i <= 14; i++) {
-			deck.push(createCard(color, i));
-		}
-	}
-}
 
 function fillHand() {
-	for (let i = 0; i < 7; i++) {
-		const number = Math.floor(Math.random() * deck.length);
-		const card = deck[number];
-		deck.splice(number, 1);
+	while (true) {
+		const card = deck.popCard();
+		if (!card) break;
 		addCardToHand(card);
 	}
 }
 
 function addCardToHand(card) {
-	hand.push(card);
+	hand.addCard(card);
 	card.addEventListener("click", playCard);
 	document.querySelector("#cardholder").appendChild(card);
 }
@@ -53,9 +81,8 @@ function addCardToHand(card) {
 function addCardToBoard(card) {
 	card.style.gridColumn = card.number;
 	card.style.gridRow = card.color;
-	console.log(card.style.gridColumn);
-	board.push(card);
-	hand.splice(hand.indexOf(card), 1);
+	console.log(hand.cards.length);
+	board.addCard(hand.getCard(card));
 	document.querySelector("#gameboard").appendChild(card);
 	card.removeEventListener("click", playCard);
 }
@@ -65,5 +92,12 @@ function playCard(e) {
 	addCardToBoard(card);
 }
 
-createDeck();
+let deck = new Deck();
+deck.fillDeck();
+deck.shuffle();
+let hand = new Deck();
+let board = new Deck();
+
 fillHand();
+// TODO add some kind of redraw function
+// hand.sortCards();
